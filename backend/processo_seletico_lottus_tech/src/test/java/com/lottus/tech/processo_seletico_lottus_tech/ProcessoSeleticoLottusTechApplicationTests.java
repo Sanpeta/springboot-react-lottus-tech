68b0e1b43f4e7013624b6c4d038647b6940cf111
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-02 00:29:42 -0300

    test: inicializado apenas

---
+++ backend/processo_seletico_lottus_tech/src/test/java/com/lottus/tech/processo_seletico_lottus_tech/ProcessoSeleticoLottusTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottus.tech.processo_seletico_lottus_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoSeleticoLottusTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
