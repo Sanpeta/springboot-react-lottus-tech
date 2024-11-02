5d19a32c0be7a5e7d1c1b26ab99bdd1d1e1a7ff8
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-02 00:10:05 -0300

    feat: criar ProductResponseDTO para retorno de dados de produtos

---
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/product/ProductResponseDTO.java
@@ -0,0 +1,20 @@
+package com.lottus.tech.processo_seletico_lottus_tech.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProductResponseDTO {
+    private Long id;
+    private String name;
+    private String code;
+    private String description;
+    private Double price;
+}
