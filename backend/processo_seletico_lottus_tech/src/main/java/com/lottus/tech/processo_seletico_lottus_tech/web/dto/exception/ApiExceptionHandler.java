372c9c593c5f2541e9b759c13996fafe026af7fb
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-02 13:27:05 -0300

    feat: criado exception para caso id do produto nao encontrado

--- backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/exception/ApiExceptionHandler.java
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/exception/ApiExceptionHandler.java
@@ -3,7 +3,6 @@ package com.lottus.tech.processo_seletico_lottus_tech.web.dto.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,10 +13,18 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessage> methodArgumentNotValid(HttpServletRequest request, MethodArgumentNotValidException e, BindingResult bindingResult) {
+    public ResponseEntity<ErrorMessage> methodArgumentNotValid(HttpServletRequest request, RuntimeException e) {
 
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
         .contentType(MediaType.APPLICATION_JSON)
-        .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) Invalido(s)", bindingResult));
+        .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) Invalido(s)"));
+    }
+
+    @ExceptionHandler(EntityNotFoundExceptionCustom.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(HttpServletRequest request, RuntimeException e) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, e.getMessage()));
     }
 }
---
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/exception/EntityNotFoundExceptionCustom.java
@@ -0,0 +1,8 @@
+package com.lottus.tech.processo_seletico_lottus_tech.web.dto.exception;
+
+public class EntityNotFoundExceptionCustom extends RuntimeException {
+    public EntityNotFoundExceptionCustom(String message) {
+        super(message);
+    }
+
+}
