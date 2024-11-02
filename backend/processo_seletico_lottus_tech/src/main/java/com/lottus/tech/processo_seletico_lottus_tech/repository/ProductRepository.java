e9126541211c7e8662ad1afde78b2fff96038a9a
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-01 23:50:07 -0300

    feat: criar ProductRepository para operações com a entidade Product

---
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.lottus.tech.processo_seletico_lottus_tech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lottus.tech.processo_seletico_lottus_tech.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
