b1130d4761b32d09e667cd022f3165670470840f
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-04 11:34:53 -0300

    fix: ajustado o nome do json no qual o response vai entregar

--- backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/order/OrderResponseDTO.java
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/web/dto/order/OrderResponseDTO.java
@@ -1,5 +1,7 @@
 package com.lottus.tech.processo_seletico_lottus_tech.web.dto.order;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,13 @@ public class OrderResponseDTO {
     private Long id;
     private String code;
     private String date;
+
+    @JsonProperty("client_name")
     private String clientName;
+
+    @JsonProperty("total_items")
     private Integer totalItems;
+
+    @JsonProperty("total_value")
     private Double totalValue;
 }
