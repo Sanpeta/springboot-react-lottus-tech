dde25a553aa43145587a85625e0df4304ee3f4e2
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-11-04 11:18:22 -0300

    feat: criano uma nova query para buscar product pelo codigo

--- backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/repository/ProductRepository.java
+++ backend/processo_seletico_lottus_tech/src/main/java/com/lottus/tech/processo_seletico_lottus_tech/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.lottus.tech.processo_seletico_lottus_tech.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lottus.tech.processo_seletico_lottus_tech.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.code = :code")
+    Optional<Product> findByCode(@Param("code") String code);
 
 }
